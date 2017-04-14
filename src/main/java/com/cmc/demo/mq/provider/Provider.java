11c69388862f258b046e92699543e01b7dfbb05e
LiChuanbin <Count.MCristo@qq.com>
2017-04-14 14:44:35 +0800

    upt

--- src/main/java/com/cmc/demo/mq/provider/Provider.java
+++ src/main/java/com/cmc/demo/mq/provider/Provider.java
@@ -6,11 +6,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cmc.mq.producer.facade.service.MQProducer;
-import com.cmc.mq.producer.service.biz.impl.MQProducerBizImplTest;
 
 public class Provider {
 
-    private static final Logger LOG = LoggerFactory.getLogger(MQProducerBizImplTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Provider.class);
 
     @Autowired
     private MQProducer mqProducer;
