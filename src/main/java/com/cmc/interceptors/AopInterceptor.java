aba8bb523ff79c1d93ffd7084505220cbef65086
Thomas Lee <Count.MCristo@qq.com>
2017-04-24 22:19:00 +0800

    upt

--- src/main/java/com/cmc/interceptors/AopInterceptor.java
+++ src/main/java/com/cmc/interceptors/AopInterceptor.java
@@ -9,52 +9,57 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+/**
+ * AOP拦截器.
+ * @author Thomas Lee
+ * @version 2017年4月23日 下午5:31:12
+ */
 @Aspect
 public class AopInterceptor {
 
-	/**
-	 * 切入点
-	 * 1. execution执行
-	 * 2. 第一个*代表返回任何类型
-	 * 3. 拦截包和类
-	 * 4. 第二个*是代表所有方法
-	 * 5. 方法中所有类型的参数
-	 */
-	@Pointcut("execution(* com.cmc.user.service.biz.impl.UserServiceImpl.*(..))")
-	private void anyMethod() {
-	}
+    /**
+     * 切入点
+     * 1. execution执行
+     * 2. 第一个*代表返回任何类型
+     * 3. 拦截包和类
+     * 4. 第二个*是代表所有方法
+     * 5. 方法中所有类型的参数
+     */
+    @Pointcut("execution(* com.cmc.user.service.biz.impl.UserServiceImpl.*(..))")
+    private void anyMethod() {
+    }
 
-	/** 前置通知，满足切入点（anyMethod）而且方法参数是一个String类型 */
-	@Before("anyMethod() && args(name)")
-	public void beforeAnyMethod(String name) {
-		System.out.println("前置通知" + name);
-	}
+    /** 前置通知，满足切入点（anyMethod）而且方法参数是一个String类型 */
+    @Before("anyMethod() && args(name)")
+    public void beforeAnyMethod(String name) {
+        System.out.println("前置通知" + name);
+    }
 
-	/** 返回通知，满足切入点，并且获取返回值 */
-	@AfterReturning(pointcut = "anyMethod()", returning = "result")
-	public void afterReturningAnyMethod(String result) {
-		System.out.println("后置通知" + result);
-	}
+    /** 返回通知，满足切入点，并且获取返回值 */
+    @AfterReturning(pointcut = "anyMethod()", returning = "result")
+    public void afterReturningAnyMethod(String result) {
+        System.out.println("后置通知" + result);
+    }
 
-	/** 后置通知 */
-	@After("anyMethod()")
-	public void afterAnyMethod() {
-		System.out.println("最终通知");
-	}
+    /** 后置通知 */
+    @After("anyMethod()")
+    public void afterAnyMethod() {
+        System.out.println("最终通知");
+    }
 
-	/** 例外通知，并且获取例外 */
-	@AfterThrowing(pointcut = "anyMethod()", throwing = "e")
-	public void throwAnyMethod(Exception e) {
-		System.out.println("例外通知" + e);
-	}
+    /** 例外通知，并且获取例外 */
+    @AfterThrowing(pointcut = "anyMethod()", throwing = "e")
+    public void throwAnyMethod(Exception e) {
+        System.out.println("例外通知" + e);
+    }
 
-	/** 环绕通知 */
-	@Around("anyMethod()")
-	public Object aroundAnyMethod(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("进入方法");
-		Object result = pjp.proceed();
-		System.out.println("退出方法");
-		return result;
-	}
+    /** 环绕通知 */
+    @Around("anyMethod()")
+    public Object aroundAnyMethod(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("进入方法");
+        Object result = pjp.proceed();
+        System.out.println("退出方法");
+        return result;
+    }
 
 }
--- src/main/java/com/cmc/interceptors/LoginInterceptor.java
+++ src/main/java/com/cmc/interceptors/LoginInterceptor.java
@@ -10,12 +10,14 @@ import org.springframework.web.servlet.ModelAndView;
 import com.cmc.common.constants.Constants;
 import com.cmc.user.facade.model.UserModel;
 
+/**
+ * 登录拦截器.
+ * @author Thomas Lee
+ * @version 2017年4月23日 下午5:29:41
+ */
 public class LoginInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = Logger.getLogger(LoginInterceptor.class);
-
-    // @Autowired
-    // private CacheManager cacheManager; 
 
     /** 
      * preHandle方法是进行处理器拦截用的，顾名思义，该方法将在Controller处理之前进行调用，SpringMVC中的Interceptor拦截器是链式的，可以同时存在 
@@ -35,15 +37,14 @@ public class LoginInterceptor implements HandlerInterceptor {
     }
 
     /** 
-    * 这个方法只会在当前这个Interceptor的preHandle方法返回值为true的时候才会执行。postHandle是进行处理器拦截用的，它的执行时间是在处理器进行处理之 
-    * 后，也就是在Controller的方法调用之后执行，但是它会在DispatcherServlet进行视图的渲染之前执行，也就是说在这个方法中你可以对ModelAndView进行操 
-    * 作。这个方法的链式结构跟正常访问的方向是相反的，也就是说先声明的Interceptor拦截器该方法反而会后调用，这跟Struts2里面的拦截器的执行过程有点像， 
-    * 只是Struts2里面的intercept方法中要手动的调用ActionInvocation的invoke方法，Struts2中调用ActionInvocation的invoke方法就是调用下一个Interceptor 
-    * 或者是调用action，然后要在Interceptor之前调用的内容都写在调用invoke之前，要在Interceptor之后调用的内容都写在调用invoke方法之后。 
-    */
+     * 这个方法只会在当前这个Interceptor的preHandle方法返回值为true的时候才会执行。postHandle是进行处理器拦截用的，它的执行时间是在处理器进行处理之 
+     * 后，也就是在Controller的方法调用之后执行，但是它会在DispatcherServlet进行视图的渲染之前执行，也就是说在这个方法中你可以对ModelAndView进行操 
+     * 作。这个方法的链式结构跟正常访问的方向是相反的，也就是说先声明的Interceptor拦截器该方法反而会后调用，这跟Struts2里面的拦截器的执行过程有点像， 
+     * 只是Struts2里面的intercept方法中要手动的调用ActionInvocation的invoke方法，Struts2中调用ActionInvocation的invoke方法就是调用下一个Interceptor 
+     * 或者是调用action，然后要在Interceptor之前调用的内容都写在调用invoke之前，要在Interceptor之后调用的内容都写在调用invoke方法之后。 
+     */
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
     }
 
     /** 
@@ -52,7 +53,6 @@ public class LoginInterceptor implements HandlerInterceptor {
      */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-
     }
 
 }
